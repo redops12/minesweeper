3f9bfa9762a43abb7372c466215c33df904097f2
unknown <rwburke@me.com>
2018-06-05 22:37:36 -0400

    overhaul

--- src/minesweeper/board.java
+++ src/minesweeper/board.java
@@ -1,20 +1,47 @@
 package minesweeper;
 
 import java.awt.*;
-import java.util.Hashtable;
-import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.util.ArrayList;
+//import java.util.Hashtable;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+
+@SuppressWarnings("serial")
 public class board extends JFrame
 {
 	static final int[] cX = {0,1,1,1,0,-1,-1,-1};
 	static final int[] cY = {1,1,0,-1,-1,-1,0,1};
     final int PREGAME = 0, INGAME = 1, ENDGAME = 2;
     final int EASY = 0, MEDIUM = 1, HARD = 2;
+    private int[][] bombs;
     private int[][] brd;
     private int[][] prevBrd;
     int gState = PREGAME;
-    Canvas canvas;
+    int diff = HARD;
+    Image unclicked;
+    Image one;
+    Image two;
+    Image three;
+    Image four;
+    Image five;
+    Image six;
+    Image seven;
+    Image eight;
+    Image bomb;
+    Image clicked;
+    Image clickedBomb;
+    Image flag;
+    Image wrongFlag;
+    ArrayList<Integer> changes = new ArrayList<Integer>();
+
     board(int diff) {
+        importImages();
+    	this.diff = diff;
         int width;
         int height;
         switch (diff){
@@ -36,24 +63,174 @@ public class board extends JFrame
                 break;
         }    
         int[][] tbrd = new int[height][width];
-        brd = tbrd;
-        copyToPrev(brd);
+        int[][] bombs = new int[height][width];
+        int[][] pbrd = new int[height][width];
+        this.bombs = bombs;
+        this.brd = tbrd;
+        this.prevBrd = pbrd;
         this.setTitle("Minesweeper");
-        this.setSize(width*41+1,height*41+1);
+        this.setSize(width*32+32,height*32+32);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
-        Image image = new Image();
-        this.setContentPane(image);
+        BoardVisual visual = new BoardVisual(width, height);
+        this.setContentPane(visual);
+        this.addMouseListener(new mouse());
+        this.addMouseMotionListener(new mouse());
         this.setVisible(true);
     }
+    
     //https://www.youtube.com/watch?v=EMu1cC2Vnis
-    public class Image extends JPanel {
+    public class BoardVisual extends JPanel {
+    	int width;
+    	int height;
+    	BoardVisual(int width, int height){
+    		this.width = width;
+    		this.height = height;
+    	}
+    	
     	public void paintComponent(Graphics g) {
-    		
+    		System.out.println(gState);
+    		if (gState == ENDGAME) {
+	    		for (int i = 0; i < brd.length; i++) {
+    				for (int j = 0; j < brd[0].length; j++) {
+    					switch(brd[i][j]) {
+    					case -3:
+    	    				g.drawImage(clickedBomb, j*32, i*32, null);
+    	    				break;
+    	    			case -2:
+    	    				g.drawImage(clicked, j*32, i*32, null);
+    	    				break;
+    	    			case -1:
+    	    				g.drawImage(bomb, j*32, i*32, null);
+    	    				break;
+    	    			case 1:
+    	    				g.drawImage(one, j*32, i*32, null);
+    	    				break;
+    	    			case 2:
+    	    				g.drawImage(two, j*32, i*32, null);
+    	    				break;
+    	    			case 3:
+    	    				g.drawImage(three, j*32, i*32, null);
+    	    				break;
+    	    			case 4:
+    	    				g.drawImage(four, j*32, i*32, null);
+    	    				break;
+    	    			case 5:
+    	    				g.drawImage(five, j*32, i*32, null);
+    	    				break;
+    	    			case 6:
+    	    				g.drawImage(six, j*32, i*32, null);
+    	    				break;
+    	    			case 7:
+    	    				g.drawImage(seven, j*32, i*32, null);
+    	    				break;
+    	    			case 8:
+    	    				g.drawImage(eight, j*32, i*32, null);
+    	    				break;
+	    				}
+	    			}
+	    		}
+    		} else if(gState == INGAME) {
+    			System.out.println(changes.toString());
+    			for (int i = 0; i < brd.length; i++) {
+    				for (int j = 0; j < brd[0].length; j++) {
+    					switch(brd[i][j]) {
+			    			case -4:
+			    				g.drawImage(flag, j*32, i*32, null);
+			    				break;
+			    			case -2:
+			    				g.drawImage(unclicked, j*32, i*32, null);
+			    				break;
+			    			case -1:
+			    				g.drawImage(unclicked, j*32, i*32, null);
+			    				break;
+			    			case 0:
+			    				g.drawImage(clicked, j*32, i*32, null);
+			    				break;
+			    			case 1:
+			    				g.drawImage(one, j*32, i*32, null);
+			    				break;
+			    			case 2:
+			    				g.drawImage(two, j*32, i*32, null);
+			    				break;
+			    			case 3:
+			    				g.drawImage(three, j*32, i*32, null);
+			    				break;
+			    			case 4:
+			    				g.drawImage(four, j*32, i*32, null);
+			    				break;
+			    			case 5:
+			    				g.drawImage(five, j*32, i*32, null);
+			    				break;
+			    			case 6:
+			    				g.drawImage(six, j*32, i*32, null);
+			    				break;
+			    			case 7:
+			    				g.drawImage(seven, j*32, i*32, null);
+			    				break;
+			    			case 8:
+			    				g.drawImage(eight, j*32, i*32, null);
+			    				break;
+			    			}
+    					}
+    				}
+    			}
+    		 else {
+    			g.setColor(Color.black);
+        		g.fillRect(0, 0, width*32, height*32);
+    			for (int i = 0; i < width; i++) {
+    				for (int j = 0; j < height; j++) {
+	    				g.drawImage(unclicked, i*32, j*32, null);
+    				}
+    			}
+    		}
     	}
     }
     
-    private void placeMines(int diff, int startX, int startY){
+    public class mouse implements MouseInputListener{
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			System.out.println("clicked");
+			clicked((int)Math.floor(e.getY()/32.0),(int)Math.floor(e.getX()/32.0));
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+    }
+    
+    private void placeMines(int startX, int startY){
         int num;
         switch (diff){
             case HARD:
@@ -70,16 +247,26 @@ public class board extends JFrame
                 break;
         }
         
+        for (int i = 0; i < brd.length; i++) {
+			for (int j = 0; j < brd[0].length; j++) {
+				brd[i][j] = -2;
+			}
+		}
+        
         for (int i = 0; i < num; i++){
             int y = (int)(Math.random()*brd.length);
             int x = (int)(Math.random()*brd[0].length);
             int disX = Math.abs(startX-x);
             int disY = Math.abs(startY-y);
-            if (brd[y][x] != -1 && disX > 1 && disY > 1)
-                brd[y][x] = -1;
-            else 
+            if (bombs[y][x] != -1 && !(disX < 1 && disY < 1)) {
+                bombs[y][x] = -1;
+            	brd[y][x] = -1;
+            }
+            else { 
                 i--;
+            }
         }
+        copyToPrev(brd);
     }
     
 	private int check(int y, int x) {
@@ -97,8 +284,15 @@ public class board extends JFrame
 	}
 	
 	private void clicked(int y, int x) {
+		System.out.println("click");
+		if (gState == PREGAME) {
+			midgame();
+			placeMines(x, y);
+		}
+		copyToPrev(brd);
 		if (brd[y][x] == -1) {
 			this.endgame();
+			brd[y][x] = -3;
 		} else {
 			brd[y][x] = check(y, x);
 		}
@@ -109,6 +303,9 @@ public class board extends JFrame
 				}					
 			}
 		}
+		trackChanges();
+		paintComponents(getGraphics());
+		printArray(brd);
 	}
 	
 	private void endgame() {
@@ -127,11 +324,45 @@ public class board extends JFrame
 		}
 	}
 	
-	void update() {
-		switch(gState) {
-		case PREGAME:
-		this.
-			
-		
+	void trackChanges() {
+		for (int i = 0; i < brd.length; i++) {
+			for (int j = 0; j < brd[0].length; j++) {
+				if(brd[i][j] != prevBrd[i][j]) {
+					changes.add(i);
+					changes.add(j);
+					System.out.println("add" + i + j);
+				}
+			}
+		}
+	}
+	
+	void importImages() {
+		try {
+			unclicked = ImageIO.read(new File("Blank.png"));
+			one = ImageIO.read(new File("One.png"));
+		    two = ImageIO.read(new File("Two.png"));
+		    three = ImageIO.read(new File("Three.png"));
+		    four = ImageIO.read(new File("Four.png"));
+		    five = ImageIO.read(new File("Five.png"));
+		    six = ImageIO.read(new File("Six.png"));
+		    seven = ImageIO.read(new File("Seven.png"));
+		    eight = ImageIO.read(new File("Eight.png"));
+		    bomb = ImageIO.read(new File("Bomb.png"));
+		    clickedBomb = ImageIO.read(new File("ClickedBomb.png"));
+		    flag = ImageIO.read(new File("Flagged.png"));
+		    clicked = ImageIO.read(new File("Clicked.png"));
+		    wrongFlag = ImageIO.read(new File("wrongFlag.png"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	void printArray(int[][] a) {
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 0; j < a[0].length-1; j++) {
+				System.out.print(a[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
